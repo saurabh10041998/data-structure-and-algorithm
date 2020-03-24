0485534dc936c2814c9b5c2feab41a4e960c2445
saurabh10041998 <shindesaurabhpict@gmail.com>
2020-03-24 11:55:45 +0530

    template for cc

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#pragma optimization_level 3
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+using namespace std;
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define int long long
+#define lsh(i) (1 << (i))
+#define rep(i, l, r) for(int i = (l); i<(r); ++i)
+#define per(i, l, r) for(int i = (l); i >= (r); --i)
+#define SPEED ios::sync_with_stdio(false); cin.tie(0)
+#define vi vector<int>
+#define vii vector<pair<int, int> >
+#define pii pair<int, int > 
+#define pb push_back
+#define SIZE (int)(2e6)+ 69
+#define __init__(t) int t; cin>>t; while(t-- > 0)
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define endl "\n"
+//mt19937 rnd(time(NULL));
+
+void solve(){
+
+}
+
+int32_t main(){
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+#endif
+  SPEED;      	
+  solve();    		
+  return 0;			
+}
